8575ed726f374dc1f15f2fce6d7cd791fe38dc78
kira <k-korni1ov@yandex.ru>
2023-08-08 19:23:06 +0400

    refact: for test TextAtom#2

--- app/src/user.InterfaceLayer/Libraries/main.library/Widgets/widget1.widget/index.tsx
+++ app/src/user.InterfaceLayer/Libraries/main.library/Widgets/widget1.widget/index.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
 // import {Icons , Icon } from "../../UI_KIT/Atoms/Atom1.atom/icons";
+import { Text, Tags } from "../../UI_KIT/Atoms/Text.atom";
 import Widget1Type from "./type";
 
 const Widget1: FunctionComponent<Widget1Type> = ({ useGetTodoQuery }) => {
@@ -7,6 +8,13 @@ const Widget1: FunctionComponent<Widget1Type> = ({ useGetTodoQuery }) => {
 
 	return <div>data from jsonplaceholder : {JSON.stringify(data)}
 			{/* <Icon width={20} height={20} color="#FAFAFA" icon={Icons.House}  /> */}
+
+			<Text type={Tags.h1}> Hello World</Text>
+			<Text type={Tags.h2}> Hello <Text type={Tags.span}> World</Text></Text>
+			<Text type={Tags.h3} color="#FFCCFF"> Lorem ipsum dolor sit amet.</Text>
+			<Text type={Tags.p}  > Lorem ipsum dolor sit amet consectetur adipisicing.</Text>
+			<Text type={Tags.p} size={20} color="#F05C00" > Lorem ipsum dolor sit amet consectetur adipisicing.</Text>
+
 	</div>;
 };
 
