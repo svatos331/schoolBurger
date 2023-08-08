839d40ccf103b7f5773ae465fe46bf8e346acb94
kira <k-korni1ov@yandex.ru>
2023-08-08 19:24:50 +0400

    fix: for test TextAtom#2

--- app/src/user.InterfaceLayer/Libraries/main.library/Widgets/widget1.widget/index.tsx
+++ app/src/user.InterfaceLayer/Libraries/main.library/Widgets/widget1.widget/index.tsx
@@ -10,7 +10,7 @@ const Widget1: FunctionComponent<Widget1Type> = ({ useGetTodoQuery }) => {
 			{/* <Icon width={20} height={20} color="#FAFAFA" icon={Icons.House}  /> */}
 
 			<Text type={Tags.h1}> Hello World</Text>
-			<Text type={Tags.h2}> Hello <Text type={Tags.span}> World</Text></Text>
+			<Text type={Tags.h2} weight={800}> Hello <Text type={Tags.span}> World</Text></Text>
 			<Text type={Tags.h3} color="#FFCCFF"> Lorem ipsum dolor sit amet.</Text>
 			<Text type={Tags.p}  > Lorem ipsum dolor sit amet consectetur adipisicing.</Text>
 			<Text type={Tags.p} size={20} color="#F05C00" > Lorem ipsum dolor sit amet consectetur adipisicing.</Text>
