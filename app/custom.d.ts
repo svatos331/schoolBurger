a12af01a6e880d2867101fc154a9c90c222e5776
COCKTAIL <aristovstas2004@gmail.com>
2023-08-09 18:36:40 +0400

    add types on dataPicker

--- app/custom.d.ts
+++ app/custom.d.ts
@@ -18,12 +18,22 @@ declare module 'react-datepicker' {
 		showTimeSelect?: boolean;
 		dateFormat?: string;
 		locale?:string|"en-ru",
-		timeFormat:string,
-		timeIntervals:number,
+		timeFormat?:string,
+		timeIntervals?:number,
 		children?:ReactNode | JSX.Element
+		showIcon?:boolean,
+		isClearable?:boolean,
+		popperClassName?:string,
+		placeholderText?:string,
+		popperPlacement?:string,
+		onCalendarClose?:() => void,
+		onCalendarOpen?:() => void,
+		popperModifiers:Array<{name?:string, options?:{offset:Array<string>, rootBoundery?:string, tether?:boolean, altAxis?:boolean}}>,
+		customInput?: forwardRef<JSX.Element | ReactNode,  {value:ReactNode, onClick : () => void}>,
+		renderCustomHeader
 		// другие пропсы
 	}
-
+//
 	class DatePicker extends Component<DatePickerProps> {}
 
 	export default DatePicker;
