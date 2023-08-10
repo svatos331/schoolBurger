2bfa1ec6526fbfa9c739323ee43dd02c0672f089
COCKTAIL <aristovstas2004@gmail.com>
2023-08-10 13:55:55 +0400

    fix onChange type in datePickerAtom

--- app/src/user.InterfaceLayer/Libraries/main.library/UI_KIT/Atoms/DataTimePicker.atom/type/index.ts
+++ app/src/user.InterfaceLayer/Libraries/main.library/UI_KIT/Atoms/DataTimePicker.atom/type/index.ts
@@ -1,5 +1,6 @@
 import {ReactDatePickerProps} from "react-datepicker";
+import * as React from "react";
 
 export interface IDatePickerProps extends ReactDatePickerProps{
-
+    onChange:(d:Date, event?: React.SyntheticEvent<any> | undefined)=>void;
 }
