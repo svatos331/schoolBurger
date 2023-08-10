a1cf4fbbb77b0075c98b3ae4971b00155c6580d8
COCKTAIL <aristovstas2004@gmail.com>
2023-08-10 12:13:17 +0400

    add types library to date-picker, delete date-picker types from custom.d.ts

--- app/src/user.InterfaceLayer/Libraries/main.library/UI_KIT/Atoms/DataTimePicker.atom/index.tsx
+++ app/src/user.InterfaceLayer/Libraries/main.library/UI_KIT/Atoms/DataTimePicker.atom/index.tsx
@@ -2,28 +2,13 @@ import {FC} from "react";
 
 import DatePicker from "react-datepicker";
 
-import 'react-datepicker/dist/react-datepicker.css';
-
-import {IDatePickerProps} from "./type";
-import {IDataTimePickerPropsDefault} from "./const";
-//@ts-ignore
-import { registerLocale, setDefaultLocale } from  "react-datepicker";
-import(`date-fns/locale/${navigator.language}`).then((locale) => {
-    registerLocale(navigator.language, locale.default || locale);
-    // Теперь можно использовать локаль в DatePicker
-}).catch((error) => {
-    console.error('Ошибка при загрузке локали:', error);
-});
+import "react-datepicker/dist/react-datepicker.css";
 import {IDatePickerProps} from "./type";
 import {IDataTimePickerPropsDefault} from "./const";
 
-
-const DataTimePicker: FC<IDatePickerProps> = ({children, dateFormat, selected, onChange, ...props}) => {
+const DataTimePicker :FC<IDatePickerProps> = ({children = null, dateFormat, selected, onChange,...props}) => {
     return (
-        <DatePicker
-            {...props}
-        >{children ? children : null}
-        </DatePicker>
+        <DatePicker onChange={onChange}/>
     );
 }
 DataTimePicker.defaultProps = IDataTimePickerPropsDefault;
