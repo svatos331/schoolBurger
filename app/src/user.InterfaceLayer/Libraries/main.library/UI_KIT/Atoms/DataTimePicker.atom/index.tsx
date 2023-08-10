83076d6182403d5dcd66dfbb216024cd771eaa62
COCKTAIL <aristovstas2004@gmail.com>
2023-08-10 11:24:43 +0400

    add doc to DataPicker, work with locale

--- app/src/user.InterfaceLayer/Libraries/main.library/UI_KIT/Atoms/DataTimePicker.atom/index.tsx
+++ app/src/user.InterfaceLayer/Libraries/main.library/UI_KIT/Atoms/DataTimePicker.atom/index.tsx
@@ -2,14 +2,23 @@ import {FC} from "react";
 
 import DatePicker from "react-datepicker";
 
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
+
 import {IDatePickerProps} from "./type";
 import {IDataTimePickerPropsDefault} from "./const";
+//@ts-ignore
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import(`date-fns/locale/${navigator.language}`).then((locale) => {
+    registerLocale(navigator.language, locale.default || locale);
+    // Теперь можно использовать локаль в DatePicker
+}).catch((error) => {
+    console.error('Ошибка при загрузке локали:', error);
+});
 
 const DataTimePicker :FC<IDatePickerProps> = ({children = null, dateFormat, selected, onChange,...props}) => {
     return (
         <DatePicker
-            locale="ru-Ru"
+            locale="ru"
             dateFormat={dateFormat}
             selected={selected}
             onChange={onChange}
