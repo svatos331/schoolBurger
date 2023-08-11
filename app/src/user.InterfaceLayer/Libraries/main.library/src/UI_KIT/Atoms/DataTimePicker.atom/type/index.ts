0ebd244bbf968e1ea911dd19f3eea0a16ca83379
COCKTAIL <aristovstas2004@gmail.com>
2023-08-11 13:05:24 +0400

    code review

--- app/src/user.InterfaceLayer/Libraries/main.library/src/UI_KIT/Atoms/DataTimePicker.atom/type/index.ts
+++ app/src/user.InterfaceLayer/Libraries/main.library/src/UI_KIT/Atoms/DataTimePicker.atom/type/index.ts
@@ -1,15 +1,15 @@
 import React from "react";
 import { ReactDatePickerProps } from "react-datepicker";
 
-type onChangeA = (
+type onChangeDateEvent = (
 	date: Date,
 	event?: React.SyntheticEvent<any> | undefined
 ) => void;
 
-type onChangeB = (date: [Date, Date]) => void;
+type onChangeIntervalEvent = (date: [Date, Date]) => void;
 
 // @ts-ignore
 export interface IDatePickerProps extends ReactDatePickerProps {
-	onChange: onChangeA | onChangeB;
+	onChange: onChangeDateEvent | onChangeIntervalEvent;
 	selectsRange?: boolean;
 }
