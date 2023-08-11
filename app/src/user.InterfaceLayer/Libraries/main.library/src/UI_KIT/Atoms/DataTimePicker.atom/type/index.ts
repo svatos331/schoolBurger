47da608fe219afb814bca69646f8575de452c9ed
COCKTAIL <aristovstas2004@gmail.com>
2023-08-11 12:59:25 +0400

    code review

--- app/src/user.InterfaceLayer/Libraries/main.library/src/UI_KIT/Atoms/DataTimePicker.atom/type/index.ts
+++ app/src/user.InterfaceLayer/Libraries/main.library/src/UI_KIT/Atoms/DataTimePicker.atom/type/index.ts
@@ -2,9 +2,10 @@ import React from "react";
 import { ReactDatePickerProps } from "react-datepicker";
 
 type onChangeA = (
-	d: Date,
+	date: Date,
 	event?: React.SyntheticEvent<any> | undefined
 ) => void;
+
 type onChangeB = (date: [Date, Date]) => void;
 
 // @ts-ignore
