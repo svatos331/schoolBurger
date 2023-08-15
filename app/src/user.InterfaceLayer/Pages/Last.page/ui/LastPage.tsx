3462117195d046e97be8c897dc925d1ee74b59e9
Ivan <94634131+veetsok@users.noreply.github.com>
2023-08-15 22:19:31 +0400

    Update LastPage.tsx

--- app/src/user.InterfaceLayer/Pages/Last.page/ui/LastPage.tsx
+++ app/src/user.InterfaceLayer/Pages/Last.page/ui/LastPage.tsx
@@ -1,14 +1,8 @@
 import React, { FC } from "react";
-import Button from "user.InterfaceLayer/Libraries/main.library/src/UI_KIT/Atoms/Button";
-import ButtonEnum from "user.InterfaceLayer/Libraries/main.library/src/UI_KIT/Atoms/Button/enum";
 
 const LastPage: FC = () => (
 	<div>
 		SvyatoslavZhilinLastPage
-		<Button type={ButtonEnum.enum_orderButton}>Оформить заказ</Button>
-		<Button type={ButtonEnum.enum_cartButton}>Вторая кнопка</Button>
-		<Button type={ButtonEnum.enum_categoryButton}>Категория</Button>
-		<Button type={ButtonEnum.enum_socialBtn}>Социальные сети</Button>
 	</div>
 );
 
